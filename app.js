21519c90d85c5b9b3b56c0409153d924db87246d
Ihsanbachtiar <ihsanbahtiar18@gmail.com>
2025-05-21 18:49:19 +0700

    add fitur create page

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var loginRouter = require('./routes/login');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -23,21 +22,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/create', indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.use('/uploads', express.static('public/uploads'));
 
 module.exports = app;
