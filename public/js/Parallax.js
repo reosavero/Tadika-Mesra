c284a5b96bce3a0c0ef717cc80072290c5a9a4da
aji883 <ajilltan@gmail.com>
2025-05-21 19:36:44 +0700

    ++++

---
+++ public/js/Parallax.js
@@ -0,0 +1,24 @@
+const gotoSection = (index, direction) => {
+  index = wrap(index);
+  animating = true;
+  let fromTop = direction === -1,
+      dFactor = fromTop ? -1 : 1,
+      tl = gsap.timeline({
+        defaults: { duration: 1.25, ease: "power1.inOut" },
+        onComplete: () => (animating = false),
+      });
+
+  if (currentIndex >= 0) {
+    gsap.set(sections[currentIndex], { zIndex: 0 });
+    tl.to(images[currentIndex], { yPercent: -15 * dFactor })
+      .set(sections[currentIndex], { autoAlpha: 0 });
+  }
+
+  gsap.set(sections[index], { autoAlpha: 1, zIndex: 1 });
+  tl.fromTo(
+    [outerWrappers[index], innerWrappers[index]],
+    { yPercent: i => (i ? -100 * dFactor : 100 * dFactor) },
+    { yPercent: 0 },
+    0
+  );
+};
