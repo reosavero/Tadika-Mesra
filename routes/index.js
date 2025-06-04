235d9398bb618da962abb4e12ff31186b50453f3
Ihsanbachtiar <ihsanbahtiar18@gmail.com>
2025-06-04 21:28:05 +0700

    the end

--- routes/index.js
+++ routes/index.js
@@ -193,4 +193,12 @@ router.get('/delete/:id', async (req, res) => {
   }
 });
 
+router.get('/about', (req, res) => {
+  res.render('about');
+});
+
+router.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
 module.exports = router;
